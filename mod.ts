abdb13b8b9c281bf51b8c810b5f43c1cf2601bb9
codehz <codehz@outlook.com>
2021-06-02 01:44:05 +0800

    fix router

--- mod.ts
+++ mod.ts
@@ -1,19 +1,43 @@
 /// <reference path="./typings/deploy.d.ts" />
 
-import { Application, Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+import { Context, Router } from "https://deno.land/x/deploy_route@0.1.0/mod.ts";
 import * as views from "./view.ts";
 import * as config from "./config.ts";
 
-const app = new Application();
-
 const router = new Router();
 
-// router.get("/default.css", )
-
-router.get("/", (ctx) => {
-  ctx.response.body = views.index({ servers: config.servers });
+router.use(async (event, next) => {
+  const time = Date.now();
+  await next();
+  console.log(
+    "[%s] %s %o +%sms",
+    event.request.method,
+    event.request.url,
+    event.params,
+    Date.now() - time,
+  );
 });
 
-app.use(router.routes());
+router.get("/", async (event) => {
+  event.respondWith(
+    new Response(
+      views.index({ servers: config.servers }),
+      {
+        headers: {
+          "content-type": "text/html",
+        },
+      },
+    ),
+  );
+});
 
-addEventListener("fetch", app.fetchEventHandler());
+router.get<{ file: string }>("/static/:file", async (event) => {
+  event.respondWith(
+    await fetch(new URL(`static/${event.params.file}`, import.meta.url)),
+  );
+});
+
+addEventListener(
+  "fetch",
+  (event) => router.dispatch(new Context(event as any)),
+);
